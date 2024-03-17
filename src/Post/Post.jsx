3c7fad225a1e448eadfcf53068d5989d859567a2
wwIrvingww <mor22781@uvg.edu.gt>
2024-03-17 15:28:29 -0600

    Post area worked

--- src/Post/Post.jsx
+++ src/Post/Post.jsx
@@ -2,8 +2,28 @@ import './post.css'
 
 export default function Post() {
     return (
-        <div>
-            
+        <div className='post'>
+            <img className='postImg'
+            src="https://lasdeliciasdevivir.net/wp-content/uploads/2019/02/Brownies-Red-Velvet-Receta-Las-Delicias-Del-Buen-Vivir.jpg" 
+            alt="" 
+            /> 
+            <div className="postInfo">
+                <div className="postCats">
+                    <span className="postCat">Category 1</span>
+                    <span className="postCat">Category 2</span>
+                </div>
+                <span className="postTitle">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </span>
+                <hr/>
+                <span className="postDate">1 hour ago</span>
+            </div>
+            <p className='postDesc'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Aliquam vero deleniti quisquam molestias, voluptatem deserunt, 
+                repudiandae aut ad repellendus culpa accusamus harum praesentium
+                commodi consectetur, hic accusantium corporis animi rerum!
+            </p>           
         </div>
     )
 }
