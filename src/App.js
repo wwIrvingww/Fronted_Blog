5960cebd3f028f30a2a8026162ffcf04e7fbf31e
wwIrvingww <mor22781@uvg.edu.gt>
2024-03-18 10:48:38 -0600

    settings page worked

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Topbar from './Components/TopBar/Topbar';
 import Home from './Pages/Home/Home';
 import Single from './Pages/Single/Single';
+import Settings from './Pages/Settings/Setting';
 import Write from './Pages/Write/Write';
 
 function App() {
   return (
   <>
    <Topbar />
-   <Write />
+   <Settings />
   </>
   );
 }
