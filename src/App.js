5c4c21bfaeb1d315f0f824c4ee4b630004209236
wwIrvingww <mor22781@uvg.edu.gt>
2024-03-18 11:44:38 -0600

    login and register pages added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from './Pages/Home/Home';
 import Single from './Pages/Single/Single';
 import Settings from './Pages/Settings/Setting';
 import Write from './Pages/Write/Write';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
 
 function App() {
   return (
   <>
    <Topbar />
-   <Settings />
+   <Register />
   </>
   );
 }
